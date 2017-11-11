2934925042a5e74fb962ac778226175e884f39a2
Luci-Lawless <lu.gmmm@gmail.com>
2017-11-11 12:52:00 +0100

    One-to-Many Models

---
+++ app/models/song.rb
@@ -0,0 +1,6 @@
+class Song < ApplicationRecord
+belongs_to :artist
+
+validates :song_title, presence: true
+validates :duration, presence: true, length: {maximum: 5} #add only accepts numbers!
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171111110844) do
+ActiveRecord::Schema.define(version: 20171111113106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,4 +23,14 @@ ActiveRecord::Schema.define(version: 20171111110844) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "songs", force: :cascade do |t|
+    t.string "song_title"
+    t.string "duration"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "artist_id"
+    t.index ["artist_id"], name: "index_songs_on_artist_id"
+  end
+
+  add_foreign_key "songs", "artists"
 end
