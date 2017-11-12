393aebf7b251f585739d89c3719e0c8cc2d1ed69
Luci-Lawless <lu.gmmm@gmail.com>
2017-11-12 12:06:41 +0100

    Implement action Create new song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,16 +1,25 @@
 class SongsController < ApplicationController
-
+before_action :set_artist, only: [:new, :create]
   def new
-    @song = Song.new
+    @song = @artist.songs.build
+  end
+
+  def create
+    @song = @artist.songs.build(song_params)
+    if @song.save
+      redirect_to artists_path, notice: "Song added!"
+    else
+      render :new
+    end
   end
 
   private
-    # def set_songs
-    #   @song = Song.find(params[:id])
-    # end
+    def set_artist
+      @artist = Artist.find(params[:artist_id])
+    end
     def song_params
       params
       .require(:song)
-      .permit(:song_title, :duration, :artist_id)
+      .permit(:song_title, :duration)
     end
 end
