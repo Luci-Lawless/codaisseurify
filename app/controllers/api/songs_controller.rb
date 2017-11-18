9411da0964db9332506e008975a5e4420330a899
Luci-Lawless <lu.gmmm@gmail.com>
2017-11-18 18:17:33 +0100

    Add API rooms show and create

---
+++ app/controllers/api/songs_controller.rb
@@ -0,0 +1,36 @@
+class Api::SongsController < ApplicationController
+before_action :set_artist, only: [:new, :create, :destroy]
+
+  def index
+    render status: 200, json: {
+      songs: Song.all
+    }.to_json
+  end
+
+  def create
+    song = Song.new(song_params)
+
+    if song.save
+      render status: 201, json: {
+        message: "Song successfully created",
+        song: song
+      }.to_json
+    else
+      render status: 422, json: {
+        errors: song.errors
+      }.to_json
+    end
+  end
+
+  private
+    def set_artist
+      @artist = Artist.find(params[:artist_id])
+    end
+
+    def song_params
+      params
+      .require(:song)
+      .permit(:song_title, :duration)
+    end
+
+end
