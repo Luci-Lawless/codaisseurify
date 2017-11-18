a5304ec3a584814959b25b92d893053711108584
Luci-Lawless <lu.gmmm@gmail.com>
2017-11-18 17:58:10 +0100

    Add API artists show

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,7 +1,40 @@
 class Api::ArtistsController < ApplicationController
+
   def index
     render status: 200, json: {
       artists: Artist.all
     }.to_json
   end
+
+  def show
+    artist = Artist.find(params[:id])
+
+    render status: 200, json: {
+      artist: artist
+    }.to_json
+  end
+
+  # def create
+  #   artist = Artist.new(artist_params)
+  #
+  #   if artist.save
+  #     render status: 201, json: {
+  #       message: "Artist successfully created",
+  #       artist: artist
+  #     }.to_json
+  #   else
+  #     render status: 422, json: {
+  #       errors: artist.errors
+  #     }.to_json
+  #   end
+  # end
+  #
+  # private
+  #
+  # def artist_params
+  #   params
+  #   .require(:artist)
+  #   .permit(:name, :genre)
+  # end
+
 end
